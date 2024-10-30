1d137c2e6e520d3389b3ae5d0e173901b743f5d7
Artem Ratushnyak <thehook.or.huc@gmail.com>
2024-10-30 18:20:43 +0300

    Refactor extension code

--- extension.js
+++ extension.js
@@ -10,17 +10,8 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Extension "mybestextension" is active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('mybestextension.whatTime', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		let time = new Date();
 		let timestr = "Current time: " + time.getHours() + ":";
 		if (time.getMinutes() < 10) timestr += "0" + time.getMinutes();
